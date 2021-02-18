e02b70bae1e4365d90a0451d9d1e2a92321f666f
Rasskris <rassoshenko.kristina@yandex.ru>
2021-02-18 18:50:18 +0300

    feat: add solution to vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+    this.alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  encrypt(msg, key) {
+    if(!msg || !key) {
+      throw new Error('Both parameters are mandatory.')
+    }
+    const normalizedMsg = msg.toUpperCase();
+    const normalizedKey = key.toUpperCase();
+    let result = '';
+
+    let keyIndex = 0;
+    for (const charOfMsg of normalizedMsg) {
+      const charOfKey = normalizedKey[keyIndex];
+      const msgIndexAtAlp = this.alp.indexOf(charOfMsg);
+      const keyIndexAtAlp = this.alp.indexOf(charOfKey);
+      if (msgIndexAtAlp !== -1) {
+        const index = (msgIndexAtAlp + keyIndexAtAlp) % this.alp.length;
+        result = `${result}${this.alp[index]}`;
+        keyIndex = ++keyIndex % key.length;
+      } else {
+        result = `${result}${charOfMsg}`;
+      }
+    }
+    return this.mode ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(msg, key) {
+    if(!msg || !key) {
+      throw new Error('Both parameters are mandatory.')
+    }
+    const normalizedMsg = msg.toUpperCase();
+    const normalizedKey = key.toUpperCase();
+    let result = '';
+
+    let keyIndex = 0;
+    for (const charOfMsg of normalizedMsg) {
+      const charOfKey = normalizedKey[keyIndex];
+      const msgIndexAtAlp = this.alp.indexOf(charOfMsg);
+      const keyIndexAtAlp = this.alp.indexOf(charOfKey);
+      if (msgIndexAtAlp !== -1) {
+        const index = (msgIndexAtAlp - keyIndexAtAlp) < 0 ?
+        (msgIndexAtAlp - keyIndexAtAlp) + this.alp.length :
+        (msgIndexAtAlp - keyIndexAtAlp);
+
+        result = `${result}${this.alp[index]}`;
+        keyIndex = ++keyIndex % key.length;
+      } else {
+        result = `${result}${charOfMsg}`;
+      }
+    }
+    return this.mode ? result : result.split('').reverse().join('');
   }
 }
 
